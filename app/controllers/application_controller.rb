23fedb7c7c826fb463c4299eea5aab77d4e196de
jared frankel <jaredfrankel10@gmail.com>
2018-01-30 21:55:44 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,19 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-
+  enable :sessions
+       set :session_secret, "fwitter_secret"
+     end
+     #Helper methods
+     helpers do
+       def logged_in?
+         !!session[:user_id]
+       end
+   
+       def current_user
+         User.find(session[:user_id])
+       end
+     end
+     get '/' do
+       erb :index
 end
