4f9ba7a47b40d229ce0bbad5b396ea3eb7e82c9b
jared frankel <jaredfrankel10@gmail.com>
2018-01-30 22:01:52 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,11 @@ class User < ActiveRecord::Base
    has_secure_password
    has_many :tweets
    validates_presence_of :username, :email, :password
- 
+
    def slug
      self.username.gsub(" ","-")
    end
- 
+
    def self.find_by_slug(slug)
      self.all.find{|n| n.slug == slug}
    end
